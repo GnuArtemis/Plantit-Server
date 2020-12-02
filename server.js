b2f47396fb6a2e100ee4c07c4472df23c533de3a
webermg <matthew.gerald.weber@gmail.com>
2020-12-02 12:05:09 -0800

    added .env config

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require("mongoose");
 const cors = require("cors")
 const compression = require("compression");
+require('dotenv').config()
 
 // Configuration
 const app = express();
